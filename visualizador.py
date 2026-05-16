1e4124ede11523023b3ed7cbb3d913558dac5739
emmekd <mimikooppler12@gmail.com>
2026-05-15 19:26:08 -0600

    visualizador
    codificación del visualizador del proyecto :)

---
+++ visualizador.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+class VisualizadorArbol:
+    def __init__(self, canvas, ancho=800, alto=500):
+        self.canvas = canvas
+        self.ancho = ancho
+        self.alto = alto
+        self.radio_nodo = 20
+
+    def dibujar(self, nodo, x, y, dx):
+        if nodo is None:
+            return
+        # Líneas a hijos
+        if nodo.izquierdo:
+            self.canvas.create_line(x, y, x - dx, y + 60, width=2)
+            self.dibujar(nodo.izquierdo, x - dx, y + 60, dx // 2)
+        if nodo.derecho:
+            self.canvas.create_line(x, y, x + dx, y + 60, width=2)
+            self.dibujar(nodo.derecho, x + dx, y + 60, dx // 2)
+        # Nodo
+        self.canvas.create_oval(x - self.radio_nodo, y - self.radio_nodo,
+                                x + self.radio_nodo, y + self.radio_nodo,
+                                fill="lightblue")
+        self.canvas.create_text(x, y, text=str(nodo.valor))
